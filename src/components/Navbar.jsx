5c1265c5acc05d10ebf91c9d4ebfa8594ab0e9da
ArshnoorSingh07 <asingh39_be23@thapar.edu>
2026-01-12 23:23:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return(
+    <div className="bg-blue-950">
+      <div className="flex flex-row justify-between">
+          <NavLink to="/">
+          <div>
+          <img src={"https://itsshahbazhere.github.io/shopEcomzy/assets/logo-CQ73aCIE.png"} 
+            alt="" width={150} height={60}/>
+          </div>
+          </NavLink>
+
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          
+          <NavLink to="/cart">
+            <div>
+              <FaShoppingCart/>
+            </div>
+          </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
