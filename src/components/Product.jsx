8334fc291de5edaa4ba0801b6375f5caef8914f1
ArshnoorSingh07 <asingh39_be23@thapar.edu>
2026-01-21 00:49:41 +0530

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -20,40 +20,51 @@ const Product = ({post}) => {
 
 
   return(
-    <div>
+    <div className="flex flex-col items-center justify-between hover:scale-110 
+    transition-all duration-300 ease-in hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] rounded-xl
+    gap-3 p-4 mt-10 ml-5 border">
       <div>
-        <p>{post.title}</p>
+        <p className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{post.title}</p>
       </div>
       
       <div>
-        <p>{post.description}</p>
+        <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ") + "..."}</p>
       </div>
 
-      <div>
-        <img src={post.image} alt=""/>
+      <div className="h-[180px]">
+        <img src={post.image} alt="" className="h-full w-full"/>
       </div>
 
-      <div>
-        <p>{post.price}</p>
+      <div className="flex justify-between gap-12 mt-5">
+        <div>
+          <p className="text-green-600 font-semibold items-center w-full mt-1">${post.price}</p>
+        </div>
+
+        {
+          cart.some((p) => p.id === post.id)
+            ? (
+                <button
+                className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px]
+                p-1 px-3 uppercase 
+                hover:bg-gray-700 hover:text-white transition-all duration-300 ease-in"
+                onClick={removeFromCart}
+                >
+                  Remove Item
+                </button>
+              )
+            : (
+                <button
+                className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px]
+                p-1 px-3 uppercase 
+                hover:bg-gray-700 hover:text-white transition-all duration-300 ease-in"
+                onClick={addToCart}
+                >
+                  Add to Cart
+                </button>
+              )
+        }
       </div>
 
-      {
-        cart.some((p) => p.id === post.id)
-          ? (
-              <button
-              onClick={removeFromCart}
-              >
-                Remove Item
-              </button>
-            )
-          : (
-              <button
-              onClick={addToCart}
-              >
-                Add to Cart
-              </button>
-            )
-      }
     </div>
   );
 };
