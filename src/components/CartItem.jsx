b75675ecfff8f0f87ccf2fc6dcc859f0b00ce952
ArshnoorSingh07 <asingh39_be23@thapar.edu>
2026-01-21 00:49:36 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -13,28 +13,29 @@ const CartItem = ({item,index}) => {
 
 
   return(
-    <div>
+    <div className="flex flex-col border border-b-black">
  
-      <div>
+      <div className="flex border gap-3 p-4 mt-5 ml-5">
 
-        <div>
-          <img src={item.image} alt=""/>
+        <div className="h-[200px] w-[200px] border flex justify-center">
+          <img src={item.image} alt="" className="h-full w-[140px]"/>
         </div>
 
-        <div>
-          <h1>{item.title}</h1>
-          <h1>{item.description}</h1>
+        <div className="flex flex-col w-[350px] ml-5">
+          <h1 className="text-gray-700 font-semibold text-md mt-1 mb-3">{item.title}</h1>
+          <h1 className="text-gray-400 font-normal text-[10px]">{item.description}</h1>
 
-          <div>
-            <p>{item.price}</p>
-            <div
+          <div className="flex justify-between gap-12 mt-5">
+            <p className="text-green-600 font-semibold items-center w-full mt-1">${item.price}</p>
+            <button
             onClick={removeFromCart}>
               <FcDeleteDatabase />
-            </div>
+            </button>
           </div>
         </div>
           
       </div>
+
 
     </div>
   );
