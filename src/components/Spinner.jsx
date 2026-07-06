b7911f9da0415d9bffe8d754f8e08d1ba1f9d00b
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-06 13:07:22 +0530

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen">
         <div className='spinner'></div>
     </div>
   )
