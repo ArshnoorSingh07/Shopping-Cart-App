5c2da0de82bee2c64b50978f58f489b742fd7979
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-31 10:05:39 +0530

    Update cartSlice.js

--- src/redux/Slices/cartSlice.js
+++ src/redux/Slices/cartSlice.js
@@ -10,8 +10,11 @@ export const CartSlice =createSlice({
         remove:(state,action)=>{
             return state.filter((item) => item.id !== action.payload);
         },
+        clearCart:()=>{
+            return [];
+        },
     }
 });
 
-export const {add,remove} = CartSlice.actions;
+export const {add,remove,clearCart} = CartSlice.actions;
 export default CartSlice.reducer;
