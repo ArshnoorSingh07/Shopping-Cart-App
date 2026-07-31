1a49269b0bd6a772c3e2688d42e85e868406c6ae
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-31 09:44:43 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,6 +7,7 @@ const Cart = () => {
   const { cart } = useSelector((state) => state);
   const [totalAmount, setTotalAmount] = useState(0);
 
+  
   useEffect(() => {
     setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
   }, [cart]);
